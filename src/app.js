29f60b8cdc58f601d3c8555a2e084304f70f50a5
qmumtaz <q.mumtaz@hotmail.com>
2024-03-14 14:36:11 +0000

    completed sprint - sort article

--- src/app.js
+++ src/app.js
@@ -1,67 +1,91 @@
-import axios from 'axios';
+import axios from "axios";
 
-const getAllArticles = () => {
-    const url = `https://nc-news-9quh.onrender.com/api/articles`;
-    return axios.get(url)
-    .then((response) => {    
-        return response.data.articles
-    })
-}
+const getAllArticles = (sortby = "created_at", order = "DESC") => {
+  let url = `https://nc-news-9quh.onrender.com/api/articles`;
+
+  if (sortby && sortby !== "all") {
+    url += `?sort_by=${sortby}`;
+    if (order) {
+      url += `&order=${order}`;
+    }
+  } else if (order) {
+    url += `?order=${order}`;
+  }
+
+
+  return axios.get(url).then((response) => {
+
+    return response.data.articles;
+  });
+};
 
 const getArticleById = (id) => {
-
-    const url = `https://nc-news-9quh.onrender.com/api/articles/${id}`;
-    return axios.get(url)
-    .then((response) => {        
-        return response.data.article
-    })
-}
+  const url = `https://nc-news-9quh.onrender.com/api/articles/${id}`;
+  return axios.get(url).then((response) => {
+    return response.data.article;
+  });
+};
 
 const getCommentByArticleId = (id) => {
-    const url = `https://nc-news-9quh.onrender.com/api/articles/${id}/comments`;
+  const url = `https://nc-news-9quh.onrender.com/api/articles/${id}/comments`;
 
-    return axios.get(url)
-    .then((response) => {
-        return response.data
-    })
-}
+  return axios.get(url).then((response) => {
+    return response.data;
+  });
+};
 
-const patchCommentByVotes = (comment_id , comment) => {
-    const url = `https://nc-news-9quh.onrender.com/api/comments/${comment_id}`;
-    const comments = {
-        inc_votes : comment.inc_votes
-    }
+const patchCommentByVotes = (comment_id, comment) => {
+  const url = `https://nc-news-9quh.onrender.com/api/comments/${comment_id}`;
+  const comments = {
+    inc_votes: comment.inc_votes,
+  };
 
-    return axios.patch(url, comments)
-           .then((response) => {
-           return response
-        })
-}
+  return axios.patch(url, comments).then((response) => {
+    return response;
+  });
+};
 
 const postCommentByArticleId = (article_id, newComment) => {
-    const url = `https://nc-news-9quh.onrender.com/api/articles/${article_id}/comments`;
-    const comment = {
-        username : newComment.username,
-        body : newComment.body
-    }
-    return axios.post(url, comment);
-
-}
+  const url = `https://nc-news-9quh.onrender.com/api/articles/${article_id}/comments`;
+  const comment = {
+    username: newComment.username,
+    body: newComment.body,
+  };
+  return axios.post(url, comment);
+};
 
 const deleteCommentById = (comment_id) => {
-    const url = `https://nc-news-9quh.onrender.com/api/comments/${comment_id}`;
+  const url = `https://nc-news-9quh.onrender.com/api/comments/${comment_id}`;
 
-    return axios.delete(url);
-}
+  return axios.delete(url);
+};
 
 const getTopic = () => {
-    const url = `https://nc-news-9quh.onrender.com/api/topics`
+  const url = `https://nc-news-9quh.onrender.com/api/topics`;
 
-    return axios.get(url)
-            .then((response) => {
-                return response.data.topics;
-    })
+  return axios.get(url).then((response) => {
+    return response.data.topics;
+  });
+};
+
+const patchArticleById = (id , newVote) => {
+  const url = `https://nc-news-9quh.onrender.com/api/articles/${id}`;
+  const votes = {
+    inc_votes : newVote.inc_votes
+  }
+
+  return axios.patch(url, votes).then((response) => {
+    return response;
+  });
 }
 
-
-export {getAllArticles , getArticleById, getCommentByArticleId , patchCommentByVotes , postCommentByArticleId , deleteCommentById , getTopic} 
+export {
+  getAllArticles,
+  getArticleById,
+  getCommentByArticleId,
+  patchCommentByVotes,
+  postCommentByArticleId,
+  deleteCommentById,
+  getTopic,
+  patchArticleById
+};
